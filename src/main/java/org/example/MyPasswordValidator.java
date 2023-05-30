ecffc1a533e5f359869964195ab4affe7ebb0b09
venterbg <venterbg@gmail.com>
2023-05-30 08:34:27 +0200

    Initial commit - password length - green refactored

--- src/main/java/org/example/MyPasswordValidator.java
+++ src/main/java/org/example/MyPasswordValidator.java
@@ -1,8 +1,13 @@
 package org.example;
 
 public class MyPasswordValidator implements PasswordValidator{
+    public static final int MINIMUM_PASSWORD_LENGTH = 8;
+    private boolean isValidLength(String password) {
+        return password.length() > MyPasswordValidator.MINIMUM_PASSWORD_LENGTH;
+    }
+
     public boolean validate(String password) {
-        return password.length() > 8;
+        return isValidLength(password);
     }
 }
 
